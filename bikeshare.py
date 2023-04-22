d5ed9e7e3cacfe3f2f2a5f48013c01ee542939ff
Frydrych <katarzyna.frydrych@arvato-scs.com>
2023-04-22 22:20:51 +0200

    refactor: Last Question was changed.

--- bikeshare.py
+++ bikeshare.py
@@ -188,7 +188,7 @@ def main():
         user_stats(df)
         ask_more_data(df)
         
-        restart = input('\nWould you like to restart the program? Enter yes or  no.\n')
+        restart = input('\nWould you like to try again? Enter yes or  no.\n')
         if restart.lower() != 'yes':
             break
 
